70583106b1e6c37d680574c6752d6d69c4fcc7ba
montealinapartes <montealinapartes@gmail.com>
2026-09-23 12:38:45 +0200

    v6.10.31.1

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME="partes-v6.10.31";
+const CACHE_NAME="partes-v6.10.31.1";
 const FILES=["./","./index.html","./styles.css","./app.js","./manifest.json","./icon.svg"];
 
 self.addEventListener("install",event=>{
